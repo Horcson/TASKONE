d8b863c901a3b8cf3fea0a48683c044ffdf3d818
Owen <ow184@student.aru.ac.uk>
2022-11-03 15:21:03 +0000

    All of it

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Array with variables
+double[] Nums = {3.4,6.7, 8.3, 9.0, 2.4};
+double sum = 0;
+
+//For Loop for calaculating sum total of Array
+for (int i = 0; i <Nums.Count(); i++)
+{
+    sum += Nums[i];
+    Console.WriteLine(sum);
+}
+//Write Line where the mean of the array is printed
+Console.WriteLine("The mean is  " + sum / Nums.Count());
+
+Console.ReadKey(); 
